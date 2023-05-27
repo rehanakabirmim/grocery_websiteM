2ceb0e9725d6cd35ab3846d972b24c40c7b81d6a
rehanakabirmim <rehanakabirmim@gmail.com>
2023-05-27 22:48:26 +0600

    first commit

---
+++ home.php
@@ -0,0 +1,15 @@
+<?php $title = "Grocery Store Website"; ?>
+
+<!-- css part -->
+<?php include("includes/css.php");?>
+
+<section class="home" id="home">
+
+<div class="content">
+    <h3>buy best <span> organic product </span> online </h3>
+    <a href="#" class="btn"> get yours now </a>
+</div>
+
+</section>
+<!-- js -->
+<?php require("includes/js.php"); ?>
